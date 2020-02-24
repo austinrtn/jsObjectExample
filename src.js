e8d9d30e82ef9cc53349904149dc2efbd3e23381
Austin Kolongowski <45217700+austinrtn@users.noreply.github.com>
2020-02-24 15:59:50 -0500

    Update src.js

--- src.js
+++ src.js
@@ -14,7 +14,7 @@ const HEIGHT = c.height;
 **********/
 
 class Rect {
-  constructor(x, y, width, height, acc, color) {
+  constructor(x, y, width, height, acc, color, isComp) {
     this.x = x;
     this.y = y;
     this.width = width;
@@ -22,6 +22,12 @@ class Rect {
     this.acc = acc;
     this.color = color;
     this.speed = 1;
+
+    this.isComp = isComp;
+    if(isComp){
+      this.keys = [87,65,83,68];
+      this.currentKey = this.keys[Math.floor(Math.random()*this.keys.length)];
+    }
   }
 
   draw() {
@@ -30,27 +36,51 @@ class Rect {
     ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 
-  move(e){
+  move(keyCode){
     if (this.speed <= player.acc) this.speed++;
 
-    if (e.keyCode === 87) player.y -= this.speed;
-    else if (e.keyCode === 65) player.x -= this.speed;
-    else if (e.keyCode === 83) player.y += this.speed;
-    else if (e.keyCode === 68) player.x += this.speed;
+    if (keyCode === 87) {
+        if((this.y - this.speed) <= 0) this.y = 0;
+        else this.y -= this.speed;
+    }
+    if (keyCode === 65) {
+      if((this.x - this.speed) <= 0) this.x = 0;
+      else this.x -= this.speed;
+    }
+    if (keyCode === 83){
+      if(((this.y + this.height) + this.speed) >= HEIGHT) this.y = HEIGHT - this.height;
+      else this.y += this.speed;
+    }
+    if (keyCode === 68){
+      if(((this.x + this.width) + this.speed) >= WIDTH) this.x = WIDTH - this.width;
+      else this.x += this.speed;
+    }
   }
 
-  double(e){
-    if(e.keyCode === 50){
+  changeSize(keyCode){
+    let sizeInc = 10;
+    if(keyCode === 73) this.height -= sizeInc;
+    if(keyCode === 74) this.width -= sizeInc;
+    if(keyCode === 75) this.height += sizeInc;
+    if(keyCode === 76) this.width += sizeInc;
+    if(keyCode === 79){
+      this.width = 10;
+      this.height = 10;
+    }
+  }
+
+  double(keyCode){
+    if(keyCode === 50){
       this.width *= 2;
       this.height *= 2;
-    } else if(e.keyCode === 49){
+    } else if(keyCode === 49){
       this.width /= 2;
       this.height /=2;
     }
   }
 }
 
-let player = new Rect(10, 10, 25, 25, 6, "#FF0000");
+let player = new Rect(10, 10, 25, 25, 2, "#FF0000", false);
 var objs = [player];
 
 ///////////////////////////////////////////////////////////////
@@ -59,14 +89,33 @@ var objs = [player];
   GAMELOOP
 **********/
 
-setInterval(gameLoop, 33);
-
 function gameLoop() {
-  ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  for (var obj of objs) obj.draw();
+  requestAnimationFrame(gameLoop);
   document.getElementById("xCord").innerHTML = "X: " + player.x;
   document.getElementById("yCord").innerHTML = "Y: " + player.y;
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  for (var obj of objs){
+    if(obj.isComp) obj.move(obj.currentKey);
+    obj.draw();
+  }
+}
+gameLoop();
 
+function addRect(){
+  var can = canvas.getBoundingClientRect();
+
+
+  let x = (event.clientX - can.left) / (can.right - can.left) * WIDTH;
+  let y = (event.clientY - can.top) / (can.bottom - can.top) * HEIGHT;
+  let width = Math.floor(15 + Math.random()*40);
+  let height = Math.floor(15 + Math.random()*40);
+
+  let colors = ["#5DADE2", "#F39C12", "#5D6D7E", "#58D68D"];
+  let color = colors[Math.floor(Math.random()*colors.length)];
+
+  console.log(x)
+  let rect = new Rect(x, y, width, height, 10, color, true);
+  objs.push(rect);
 }
 
 //////////////////////////////////////////////
@@ -85,8 +134,9 @@ function changeColor(){
 }
 
 document.addEventListener("keydown", function(e) {
-  player.move(e);
-  player.double(e);
+  player.move(e.keyCode);
+  player.changeSize(e.keyCode);
+  player.double(e.keyCode);
 });
 
 document.addEventListener("keyup", function(e) {
